77526eb94545a71e1b26a7d5d8cc56f68acd688b
Victorium2010 <162732373+Victorium2010@users.noreply.github.com>
2026-07-14 13:25:25 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,48 @@
+let db;
 
+function openDatabase() {
+
+    return new Promise((resolve, reject) => {
+
+        const request = indexedDB.open("YouTubeRandomizerDB", 1);
+
+        request.onupgradeneeded = function (event) {
+
+            db = event.target.result;
+
+            if (!db.objectStoreNames.contains("channels")) {
+
+                db.createObjectStore("channels", {
+                    keyPath: "id"
+                });
+
+            }
+
+            if (!db.objectStoreNames.contains("videos")) {
+
+                db.createObjectStore("videos", {
+                    keyPath: "videoId"
+                });
+
+            }
+
+        };
+
+        request.onsuccess = function (event) {
+
+            db = event.target.result;
+            console.log("Base de datos abierta");
+
+            resolve();
+
+        };
+
+        request.onerror = function () {
+
+            reject("Error al abrir la base de datos");
+
+        };
+
+    });
+
+}
