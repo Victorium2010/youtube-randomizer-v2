73c00f7535eb3853536b603a025ea964479bf203
Victorium2010 <162732373+Victorium2010@users.noreply.github.com>
2026-07-15 00:59:49 +0200

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -35,19 +35,3 @@ async function getChannelInfo(input) {
     };
 
 }
-
-    const response = await fetch(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&q=${encodeURIComponent(query)}&key=${API_KEY}`
-    );
-
-    const data = await response.json();
-
-    if (!data.items || data.items.length === 0) {
-        throw new Error("Canal no encontrado");
-    }
-
-    return {
-        id: data.items[0].id.channelId,
-        name: data.items[0].snippet.title
-    };
-}
