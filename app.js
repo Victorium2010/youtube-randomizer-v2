c445fb9088f884f54e9b5c049f1b315a375624b3
Victorium2010 <162732373+Victorium2010@users.noreply.github.com>
2026-07-14 13:52:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,37 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         await openDatabase();
 
-        await addChannel({
-            id: "prueba",
-            name: "Canal de prueba"
-        });
-
-        console.log("Canal guardado correctamente");
+        console.log("Aplicación iniciada");
 
     } catch (error) {
 
         console.error(error);
 
     }
+
+    const addButton = document.getElementById("addChannelBtn");
+
+    addButton.addEventListener("click", async () => {
+
+        const input = document.getElementById("channelInput").value;
+
+        try {
+
+            const channel = await getChannelInfo(input);
+
+            alert(
+                "Canal encontrado:\n\n" +
+                channel.name +
+                "\n" +
+                channel.id
+            );
+
+        } catch (error) {
+
+            alert(error.message);
+
+        }
+
+    });
 
 });
