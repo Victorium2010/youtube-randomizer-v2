38ba940a5f7694338c659d040286b32ea535e6f2
Victorium2010 <162732373+Victorium2010@users.noreply.github.com>
2026-07-14 13:27:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", async () => {
 
+    try {
+
+        await openDatabase();
+
+        console.log("Aplicación iniciada");
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+});
